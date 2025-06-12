baaef4782da4c34bca0e3ba81f5341172d28a6b0
Babatunde <orsheenxi@gmail.com>
2025-06-12 11:48:40 +0100

    Initial files: Folder structure

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+
+  const productName = null;
+
+  root.innerHTML = `
+        <main>
+            <div class="container">
+             <p>INTRODUCING</p>
+            <h1>${productName}</h1>
+            </div>
+           
+        </main>
+    `;
+});
